1dbb026fdc7f2d80e29d723b5b755fc957f98afe
marif <marif@DESKTOP-QHECBIK>
2024-01-16 17:45:11 +0300

    готово мб

--- main.c
+++ main.c
@@ -1,16 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+#include "world.h"
 #include "render.h"
-#include  "obj.h"
 
 world_t *myworld;
-static struct camera camera;
-vec2_t* screen;
+camera_t camera;
+
+char screen[H_SCREEN * W_SCREEN]
+vec2_t vecarr[];
 
 int main(int argc, char* argv[]){
     char ref[20] = "obj";
-    myworld = read_obj_dir(ref);
+    myworld = read_obj_dir();
     camera.pos.x =-4;camera.pos.y = 0;camera.pos.z = 0;camera.rotat.x = 1;camera.rotat.y = 0;camera.rotat.z = 0;
     //rotate_obj(&myworld->objs[2],M_PI/4, Xasis);
     int sum = 0;
@@ -18,25 +17,25 @@ int main(int argc, char* argv[]){
     {
         sum += myworld->objs[i].c_v;
     }
-    screen = calloc(sum,sizeof(vec2_t));
+    vecarr = calloc(sum,sizeof(vec2_t));
     getchar();
     float count = 0.1;
     while (1)
     {
-        rotate_obj(&myworld->objs[0],count * M_PI/180, Zasis);
+        rotate_obj( &myworld->objs[0], count * M_PI/180, Zasis );
         //rotate_obj(&myworld->objs[0],count * M_PI/180, Xasis);
         //move_obj(&myworld->objs[0], tp);
-        clean();
-        screen_proection();
+        clean_screen( screen );
+        screen_proection( myworld, camera, vecarr );
         for (size_t i = 0; i < myworld->c_objs; i++)
         {
             obj_t* obj_t = &myworld->objs[i];
             for (size_t l = 0; l < obj_t->c_l; l++)
             {
-                print_line2d(&screen[obj_t->l[l].a], &screen[obj_t->l[l].b]);
+                print_line2d(&vecarr[obj_t->l[l].a], &vecarr[obj_t->l[l].b], screen );
             }
         }
-        puts(window_bufer);
+        puts( screen );
     }
 return 0;
 }

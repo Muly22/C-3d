bad96b74da7a3f20a475a11c396941cf41789e81
Muly/Morryaland <110240465+Muly22@users.noreply.github.com>
2024-01-19 12:19:15 +0300

    Update render.h
    comments added

--- include/render.h
+++ include/render.h
@@ -1,13 +1,20 @@
 #ifndef render_h
 #define render_h
 
+#include "include/3d_mach.h"
 #include "include/world.h"
 
+/* console window resolution */
 #define H_SCREEN 30
 #define W_SCREEN 120
 
+/* filling the screen buffer with spaces */
 void clean_screen( char screen[] );
+
+/* 3d to 2d projection */
 void screen_proection( world_t *myworld, camera_t camera, vec2_t screen[] );
+
+/* drawing lines on the screen buffer */
 void print_line2d( const vec2_t a, const vec2_t b, char screen[] );
 
 #endif

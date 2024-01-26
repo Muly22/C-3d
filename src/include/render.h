c60b112549c79a6a18b8725fea1b97bc0aa1ec80
Muly/Morryaland <110240465+Muly22@users.noreply.github.com>
2024-01-26 11:54:12 +0300

    Update render.h
    коменты

--- src/include/render.h
+++ src/include/render.h
@@ -3,7 +3,10 @@
 
 #include "3d_mach.h"
 #include "world.h"
+
+/* compile for the windows “we need to cut it out later because it’s a crutch” */
 #define win
+
 /* console window resolution */
 #define H_SCREEN 149
 #define W_SCREEN 250
@@ -17,10 +20,13 @@ void screen_proection( world_t *myworld, camera_t *camera, vec2_t screen[] );
 /* drawing lines on the screen buffer */
 void print_line2d( const vec2_t a, const vec2_t b, char screen[] );
 
-
+/* if the code is executed on a window then initialize gotoxy */
 #ifdef win
 #include <windows.h>
+
+/* function to move the cursor to a specific position */
 void gotoxy(int x, int y);
+
 #endif
 
 #endif
